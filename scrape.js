bfd74534806210af705028cb4287a3af02b386db
Thomas Lee <tlee9595@gmail.com>
2019-07-15 19:10:11 -0400

    Working scraping

--- scrape.js
+++ scrape.js
@@ -5,44 +5,9 @@ request('https://www.baseball-reference.com/leagues/MLB/2019.shtml', (error, res
     if (!error && response.statusCode == 200) {
         const $ = cheerio.load(html);
 
-        const divs = $('.section_heading');
-        const divString = divs.html();
-        let isOnFirstDiv = false;
-        let firstIndex = 0;
-        let foundDivs = [];
-        for (let x = 0; x < divString.length; x++) {
-            if (isDiv(divString, x)) {
-                if (isOnFirstDiv) {
-                    foundDivs.push(divString.slice(firstIndex, x));
-                    isOnFirstDiv = false;
-                } 
-                else {
-                    firstIndex = x;
-                    isOnFirstDiv = true;
-                }
-            }
-        }
-
-        for (let x = 0; x < foundDivs.length; x++) {
-            const subString = foundDivs[x].substring(0, 3);
-            console.log(subString);
-
-            if (subString === 'td c') {
-                console.log('yeeeeeeee' + foundDivs[x]);
-            }
-
-        }
-        
-        //console.log('found divs: ' + foundDivs.length);
+        $('.section_heading').each((index, element) => {
+            const $element = $(element);
+            console.log($element.text());
+        });
     }
 });
-
-function isDiv(divString, index) {
-    // checking for divs...
-
-    if (divString[index] === 'd' && divString[index + 1] === 'i' && divString[index + 2] === 'v') {
-        return true;
-    }
-
-    return true;
-}
