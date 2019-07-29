5f4aa7fc229021805dd3e97e14b37c9f821cb3fd
Thomas Lee <tlee9595@gmail.com>
2019-07-29 11:41:58 -0400

    Added Fantasy Football Scraping code

--- scrape.js
+++ scrape.js
@@ -1,30 +1,63 @@
 const request = require('request');
 const cheerio = require('cheerio');
-const requestPromise = require('request-promise');
 
-var options = {
-    uri: 'https://www.baseball-reference.com/leagues/MLB/2019.shtml',
-    headers: {
-        'User-Agent': 'request'
+var ffURL = 'https://www.pro-football-reference.com/years/2018/fantasy.htm';
+runFFURL({ uri: ffURL, headers: { 'User-Agent': 'request' } });
+
+function runFFURL(options) {
+    request(options, (error, response, html) => {
+        if (!error && response.statusCode === 200) {
+            const $ = cheerio.load(html);
+            console.log('success');
+
+            const allTableRows = $('tbody').find('tr');
+            allTableRows.each((index, trElement) => {
+                const $trElement = $(trElement);
+                //console.log({ trElement: $trElement.html() });
+
+                $trElement.find('td').each((i, tdElement) => {
+                    const $tdElement = $(tdElement);
+                    const attribute = $tdElement.attr('data-stat');
+                    let value = $tdElement.html();
+
+                    if (attribute === 'player' || attribute === 'team') {
+                        value = getHtmlValue(value);
+                    }
+
+                    console.log({ attribute, value });
+                });
+            });
+        }
+    });
+}
+
+function getHtmlValue(htmlString) {
+    let firstSlice = 0;
+    let secondSlice = 0;
+
+    for (let x = 0; x < htmlString.length; x++) {
+        if (firstSlice === 0 && htmlString.charAt(x) === '>') {
+            firstSlice = x + 1;
+        }
+        else if (x !== 0 && secondSlice === 0 && htmlString.charAt(x) === '<') {
+            secondSlice = x;
+            break;
+        }
     }
+
+    return htmlString.slice(firstSlice, secondSlice);
 }
 
-var BASEURL = 'https://www.baseball-reference.com/players/';
-
-var optionsListOfPlayersA = {
-    uri: BASEURL + 'a/'
-}
-var alphabets = 'abcdefghijklmnopqrstuvwxyz';
-
+const BASEURL  = 'https://www.baseball-reference.com/players/';
+const alphabets = 'abcdefghijklmnopqrstuvwxyz';
 var AllMLBPlayers = {};
-
 for (let x = 0; x < alphabets.length; x++) {
     const letter = alphabets[x];
     var optionsListOfPlayers = {
         uri: BASEURL + letter
     }
 
-    runRequest(optionsListOfPlayers, letter)
+    //runRequest(optionsListOfPlayers, letter)
 }
 
 function runRequest(options, letter) {
@@ -73,48 +106,11 @@ function runRequest(options, letter) {
                     //console.log(tableString);
                 }
             })
-            console.log(AllMLBPlayers); // send it to database
         }
        // console.log({listOfPlayersWithLastNameA})
+
+       //console.log(AllMLBPlayers); // send it to database
     });
 
     return listOfPlayersWithLastNameA;
 }
-
-
-
-/*
-request(options, (error, response, html) => {
-    if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
-
-        let teamBattingTable = {};
-
-        $('.table_outer_container').each((index, element) => {
-            const $element = $(element);
-            const tableString = $element.text();
-
-            $element.find('tr').each((i, tr) => {
-                const $tr = $(tr);
-
-                const teamname = '';
-
-                $tr.find('th a').each((j, th) => {
-                    const $th = $(th);
-                    teamName = $th.html();
-                    teamBattingTable[teamName] = {};
-                });
-
-                $tr.find('td').each((j, td) => {
-                    const $td = $(td);
-
-                    teamBattingTable[teamName][$td.attr('data-stat')] = $td.html();
-                });
-            })
-        });
-
-        console.log(teamBattingTable);
-    }
-});
-
-*/
